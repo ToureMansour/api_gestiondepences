3b98857243c3365b39dc519040856905706c26de
Mansour <167453336+developer3iwaju@users.noreply.github.com>
2026-06-26 11:04:03 +0100

    feat: register UUID validation middleware in application

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
+            'uuid' => \App\Http\Middleware\ValidateUuid::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
